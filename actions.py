8be5047dca00cf6a9e2209ae0049beb06c52d913
Denis Jackman <denis_jackman@hotmail.com>
2022-06-27 10:30:36 +0100

    Updated so we can start lesson 6

--- actions.py
+++ actions.py
@@ -4,34 +4,43 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Optional, Tuple, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from engine import Engine
     from entity import Entity
 
 
-class Action:  # pylint: disable=R0903
+class Action:
     '''
         This is the generic action class
     '''
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def __init__(self, entity: Entity) -> None:
+        super().__init__()
+        self.entity = entity
+
+    @property
+    def engine(self) -> Engine:
+        """Return the engine this action belongs to."""
+        return self.entity.gamemap.engine
+
+    def perform(self) -> None:
         """Perform this action with the objects needed to determine its scope.
 
-        `engine` is the scope this action is being performed in.
+        `self.engine` is the scope this action is being performed in.
 
-        `entity` is the object performing the action.
+        `self.entity` is the object performing the action.
 
         This method must be overridden by Action subclasses.
         """
         raise NotImplementedError()
 
 
-class EscapeAction(Action):  # pylint: disable=R0903
+class EscapeAction(Action):
     '''
         this is the class for the escape button
     '''
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self) -> None:
         raise SystemExit()
 
 
@@ -39,13 +48,23 @@ class ActionWithDirection(Action):
     '''
         class action with direction
     '''
-    def __init__(self, dx: int, dy: int):
-        super().__init__()
+    def __init__(self, entity: Entity, dx: int, dy: int):
+        super().__init__(entity)
 
         self.dx = dx
         self.dy = dy
 
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    @property
+    def dest_xy(self) -> Tuple[int, int]:
+        """Returns this actions destination."""
+        return self.entity.x + self.dx, self.entity.y + self.dy
+
+    @property
+    def blocking_entity(self) -> Optional[Entity]:
+        """Return the blocking entity at this actions destination.."""
+        return self.engine.game_map.get_blocking_entity_at_location(*self.dest_xy)
+
+    def perform(self) -> None:
         raise NotImplementedError()
 
 
@@ -53,43 +72,39 @@ class MeleeAction(ActionWithDirection):
     '''
         class melee action
     '''
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
-        target = engine.game_map.get_blocking_entity_at_location(dest_x,
-                                                                 dest_y)
+    def perform(self) -> None:
+        target = self.blocking_entity
         if not target:
-            return  # No entity to attack.
+            return
+        # No entity to attack.
 
-        print(f"You kick the {target.name}, much to its annoyance!")
+        print(f"You kick the {target.name}, much to it's annoyance!")
 
 
 class MovementAction(ActionWithDirection):  # pylint: disable=R0903
     '''
         this is the class for a movement action
     '''
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
+    def perform(self) -> None:
+        dest_x, dest_y = self.dest_xy
 
-        if not engine.game_map.in_bounds(dest_x, dest_y):
-            return  # Destination is out of bounds.
-        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
-            return  # Destination is blocked by a tile.
-        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-            return  # Destination is blocked by an entity.
-        entity.move(self.dx, self.dy)
+        if not self.engine.game_map.in_bounds(dest_x, dest_y):
+            return
+            # Destination is out of bounds.
+        if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return
+            # Destination is blocked by a tile.
+        if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return
+            # Destination is blocked by an entity.
+        self.entity.move(self.dx, self.dy)
 
 
 class BumpAction(ActionWithDirection):
     '''
         class bump action
     '''
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
-
-        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-            return MeleeAction(self.dx, self.dy).perform(engine, entity)
-
-        return MovementAction(self.dx, self.dy).perform(engine, entity)
+    def perform(self) -> None:
+        if self.blocking_entity:
+            return MeleeAction(self.entity, self.dx, self.dy).perform()
+        return MovementAction(self.entity, self.dx, self.dy).perform()
--- main.py
+++ main.py
@@ -7,11 +7,9 @@ import copy
 import tcod
 
 
-
 from engine import Engine
 import entity_factories
 
-from input_handlers import EventHandler
 from procgen import generate_dungeon
 
 
@@ -38,23 +36,21 @@ def main() -> None:  # pylint: disable=R0914
                 tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
-
     player = copy.deepcopy(entity_factories.player)
 
-    game_map = generate_dungeon(
+    engine = Engine(player=player)
+
+    engine.game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_min_size=room_min_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
-        player=player
+        engine=engine,
     )
 
-    engine = Engine(event_handler=event_handler,
-                    game_map=game_map,
-                    player=player)
+    engine.update_fov()
 
     with tcod.context.new(
         width=screen_width,
@@ -66,9 +62,8 @@ def main() -> None:  # pylint: disable=R0914
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             engine.render(console=root_console, context=context)
-            events = tcod.event.wait()
 
-            engine.handle_events(events)
+            engine.event_handler.handle_events()
 
 
 if __name__ == "__main__":
