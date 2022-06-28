2a6d206a7123db3a5e458d5227d72eec5be75d95
Denis Jackman <denis_jackman@hotmail.com>
2022-06-28 16:10:47 +0100

    Added monster AI and the ability to die

--- entity_factories.py
+++ entity_factories.py
@@ -1,23 +1,35 @@
 '''
     This is a factory for entities
 '''
-from entity import Entity
+from components.ai import HostileEnemy
+from components.fighter import Fighter
+from entity import Actor
 from colours import (
     WHITE,
     ORC_GREEN,
     TROLL_GREEN
 )
+player = Actor(
+    char="@",
+    color=WHITE,
+    name="Player",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=30, defense=2, power=5),
+)
 
-player = Entity(char="@",
-                color=WHITE,
-                name="Player",
-                blocks_movement=True)
 
-orc = Entity(char="o",
-             color=ORC_GREEN,
-             name="Orc",
-             blocks_movement=True)
-troll = Entity(char="T",
-               color=TROLL_GREEN,
-               name="Troll",
-               blocks_movement=True)
+orc = Actor(
+    char="o",
+    color=ORC_GREEN,
+    name="Orc",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=10, defense=0, power=3),
+)
+
+troll = Actor(
+    char="T",
+    color=TROLL_GREEN,
+    name="Troll",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=16, defense=1, power=4),
+)
