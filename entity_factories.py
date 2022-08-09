670f7afef22222b254be44c1c774e46b07f03ae2
Denis Jackman <denis_jackman@hotmail.com>
2022-08-10 00:09:04 +0100

    tutorial part 9 scrolls and potions

--- entity_factories.py
+++ entity_factories.py
@@ -2,21 +2,16 @@
     This is a factory for entities
 '''
 from components.ai import HostileEnemy
-from components.consumable import HealingConsumable
+from components import consumable
 from components.fighter import Fighter
 from components.inventory import Inventory
 from entity import Actor, Item
 
-from colours import (
-    WHITE,
-    ORC_GREEN,
-    TROLL_GREEN,
-    HEALING_POTION
-)
+import colours
 
 player = Actor(
     char="@",
-    color=WHITE,
+    color=colours.WHITE,
     name="Player",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=30, defense=2, power=5),
@@ -25,7 +20,7 @@ player = Actor(
 
 orc = Actor(
     char="o",
-    color=ORC_GREEN,
+    color=colours.ORC_GREEN,
     name="Orc",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=10, defense=0, power=3),
@@ -34,16 +29,37 @@ orc = Actor(
 
 troll = Actor(
     char="T",
-    color=TROLL_GREEN,
+    color=colours.TROLL_GREEN,
     name="Troll",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=16, defense=1, power=4),
     inventory=Inventory(capacity=0),
 )
 
+confusion_scroll = Item(
+    char="~",
+    color=colours.CONFUSION_SCROLL,
+    name="Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
+
+fireball_scroll = Item(
+    char="~",
+    color=colours.RED,
+    name="Fireball Scroll",
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
+)
+
 health_potion = Item(
     char="!",
-    color=HEALING_POTION,
+    color=colours.HEALING_POTION,
     name="Health Potion",
-    consumable=HealingConsumable(amount=4),
+    consumable=consumable.HealingConsumable(amount=4),
+)
+
+lightning_scroll = Item(
+    char="~",
+    color=colours.LIGHTNING_SCROLL,
+    name="Lightning Scroll",
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
 )
