faab08dc968c55b57f68d3ad4596031590b740fd
Denis Jackman <denis_jackman@hotmail.com>
2022-08-09 09:43:41 +0100

    daily commit - part 8 - health potion

--- game_map.py
+++ game_map.py
@@ -8,7 +8,7 @@ from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 import numpy as np  # type: ignore
 from tcod.console import Console
 
-from entity import Actor
+from entity import Actor, Item
 import tile_types
 
 if TYPE_CHECKING:
@@ -57,6 +57,15 @@ class GameMap:
             for entity in self.entities
             if isinstance(entity, Actor) and entity.is_alive
         )
+
+    @property
+    def items(self) -> Iterator[Item]:
+        '''
+            items property 
+        '''
+        yield from (entity for entity in self.entities if isinstance(entity, Item))
+
+
 
     def get_blocking_entity_at_location(
         self, location_x: int, location_y: int,
